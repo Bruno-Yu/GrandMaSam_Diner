517b234a18f7693e0815c0e24792580b3ccca8fd
Bruno-Yu <jackhellowin@gmail.com>
2022-04-09 14:40:52 +0800

    adjusted abit

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { required, email, min } from '@vee-validate/rules';
 import { localize, setLocale } from '@vee-validate/i18n';
 import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 import ModalMessage from '@/components/ModalMessage.vue';
+// import PageLoading from '@/components/PageLoading.vue';
 import App from './App.vue';
 import router from './router';
 
@@ -24,6 +25,7 @@ setLocale('zh_TW');
 const app = createApp(App);
 app.component('VForm', Form);
 app.component('ModalMessage', ModalMessage);
+// app.component('PageLoading', PageLoading);
 app.component('VField', Field);
 app.component('ErrorMessage', ErrorMessage);
 app.use(router);
