c052236cbb28e16d00796cfb9949cce83753b0a8
Bruno-Yu <jackhellowin@gmail.com>
2022-04-12 19:00:45 +0800

    updated over paid suceess page

--- src/views/frontend/PaidSuccess.vue
+++ src/views/frontend/PaidSuccess.vue
@@ -5,20 +5,23 @@
     :can-cancel="true"
     :is-full-page="false"
   ></PageLoading>
-  <div class="container">
+  <div class="container paid-success shadow mb-2 p-1">
     <div class="my-5">
-      <p class="fs-2 fw-bold text-center">
-        <i class="bi bi-check2-circle text-success fw-bolder"></i> 付款成功
+      <p class="fs-2 fw-bold text-dark text-lg-light text-center">
+        <i class="bi bi-check2-circle text-success fw-bolder"></i>
+        付款成功
       </p>
-      <div class="mb-3 d-flex justify-content-center">
-        <p class="mb-3 fw-bold">訂單編號: {{ orderID }}</p>
-      </div>
-      <p class="text-center mb-3">謝謝惠顧，願上帝與您同在!</p>
       <div class="d-flex justify-content-center">
-        <button type="button" class="btn btn-danger my-5">
+        <p class="fw-bold text-light">訂單編號: {{ orderID }}</p>
+      </div>
+      <div class="d-flex justify-content-center">
+        <button
+          type="button"
+          class="btn btn-warning border-1 border fw-bold border-light my-5"
+        >
           <router-link
             to="/productsView"
-            class="text-decoration-none link-light"
+            class="text-decoration-none fs-5 link-light"
           >
             回到商城
           </router-link>
@@ -47,3 +50,11 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.paid-success {
+  background-image: url(@/assets/images/paid_success.jpg);
+  background-position: center center;
+  background-size: cover;
+}
+</style>
